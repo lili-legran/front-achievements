d5f76b5b234e353b514519f0691159d6682da218
Lili Legran <lili_akhunyanova@icloud.com>
2020-03-12 18:31:41 +0300

    fix: components

--- src/components/header/skill-button/skill-button.js
+++ src/components/header/skill-button/skill-button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './skill-button.scss';
-import JsIcon from '../icons/js-icon';
+import JsIcon from '../../icons/js-icon';
 
 class SkillButton extends React.Component {
   render() {
