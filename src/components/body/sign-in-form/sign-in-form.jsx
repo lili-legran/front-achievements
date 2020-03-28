4f07f48a7d3c4a4e2e9508a6fcd88bff134cfe20
Lili Legran <lili_akhunyanova@icloud.com>
2020-03-28 13:23:12 +0300

    fix: sign in, sign up components

--- src/components/body/sign-in-form/sign-in-form.jsx
+++ src/components/body/sign-in-form/sign-in-form.jsx
@@ -6,21 +6,23 @@ import './sign-in-form.scss';
 class SignInForm extends React.Component {
   render() {
     return (
-      <div className='sign-in-form'>
-        <label className='sign-in-form__label sign-in-form__login-label' htmlFor='login'>
-          Login
-          <input type='text' className='sign-in-form__input sign-in-form__login' id='login' />
-        </label>
-        <label className='sign-in-form__label sign-in-form__email-label' htmlFor='email'>
-          E-mail
-          <input type='text' className='sign-in-form__input sign-in-form__email' id='email' />
-        </label>
-        <label className='sign-in-form__label sign-in-form__pass-label' htmlFor='pass'>
-          Password
-          <input type='password' className='sign-in-form__input sign-in-form__pass' id='pass' />
-        </label>
-        <input type='submit' className='sign-in-form__submit' value='Sign in' />
-      </div>
+      <form>
+        <div className='sign-in-form'>
+          <label className='sign-in-form__label sign-in-form__login-label' htmlFor='sign-in-login'>
+            Login
+            <input type='text' className='sign-in-form__input sign-in-form__login' id='sign-in-login' />
+          </label>
+          <label className='sign-in-form__label sign-in-form__email-label' htmlFor='sign-in-email'>
+            E-mail
+            <input type='text' className='sign-in-form__input sign-in-form__email' id='sign-in-email' />
+          </label>
+          <label className='sign-in-form__label sign-in-form__pass-label' htmlFor='sign-in-pass'>
+            Password
+            <input type='password' className='sign-in-form__input sign-in-form__pass' id='sign-in-pass' autoComplete='on' />
+          </label>
+          <input type='submit' className='sign-in-form__submit' value='Sign in' />
+        </div>
+      </form>
     );
   }
 }
--- src/components/body/sign-up-form/sign-up-form.jsx
+++ src/components/body/sign-up-form/sign-up-form.jsx
@@ -6,17 +6,19 @@ import './sign-up-form.scss';
 class SignUpForm extends React.Component {
   render() {
     return (
-      <div className='sign-up-form'>
-        <label className='sign-up-form__label sign-up-form__logup-label' htmlFor='login'>
-          Login
-          <input type='text' className='sign-up-form__input sign-up-form__login' id='login' />
-        </label>
-        <label className='sign-up-form__label sign-up-form__pass-label' htmlFor='pass'>
-          Password
-          <input type='password' className='sign-up-form__input sign-up-form__pass' id='pass' />
-        </label>
-        <input type='submit' className='sign-up-form__submit' value='Sign up' />
-      </div>
+      <form>
+        <div className='sign-up-form'>
+          <label className='sign-up-form__label sign-up-form__logup-label' htmlFor='sign-up-login'>
+            Login
+            <input type='text' className='sign-up-form__input sign-up-form__login' id='sign-up-login' />
+          </label>
+          <label className='sign-up-form__label sign-up-form__pass-label' htmlFor='sign-up-pass'>
+            Password
+            <input type='password' className='sign-up-form__input sign-up-form__pass' id='sign-up-pass' autoComplete='on' />
+          </label>
+          <input type='submit' className='sign-up-form__submit' value='Sign up' />
+        </div>
+      </form>
     );
   }
 }
