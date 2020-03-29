2616567811bae136cb14eb4eddb52139f75be76a
Lili Legran <lili_akhunyanova@icloud.com>
2020-03-29 21:24:09 +0300

    feat: toggle achievement

--- src/components/body/achievement/achievement.jsx
+++ src/components/body/achievement/achievement.jsx
@@ -2,24 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './achievement.scss';
 
-const Achievement = (props) => {
-  const { title, description, mark } = props;
-  return (
-    <div className='achievement'>
-      <div className='achievement__designation'>
-        <div className='achievement__title'>
-          { title }
+class Achievement extends React.Component {
+  state = {
+    isActive: false
+  }
+
+  toggleAchievement = () => {
+    this.setState((prevState) => ({
+      isActive: !prevState.isActive
+    }));
+  };
+
+  render() {
+    const { title, description, mark } = this.props;
+    const { isActive } = this.state;
+    return (
+      <div className={`achievement ${isActive ? 'achievement__active' : ''}`} onClick={this.toggleAchievement}>
+        <div className='achievement__designation'>
+          <div className='achievement__title'>
+            { title }
+          </div>
+          <div className='achievement__description'>
+            { description }
+          </div>
         </div>
-        <div className='achievement__description'>
-          { description }
+        <div className='achievement__mark'>
+          { mark }
         </div>
       </div>
-      <div className='achievement__mark'>
-        { mark }
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Achievement.propTypes = {
   title: PropTypes.string.isRequired,
