c3217a1dabfa12b9177f26b1b505e64fa80c5d09
Lili Legran <lili_akhunyanova@icloud.com>
2020-03-31 23:25:42 +0300

    feat: loading component

--- src/components/skill-check.jsx
+++ src/components/skill-check.jsx
@@ -7,11 +7,12 @@ import {
   Switch
 } from 'react-router-dom';
 import ErrorPage from './body/error-page/error-page';
-import './skill-check.scss';
 import HeaderBox from './header/header-box/header-box';
 import AchievementsBox from './body/achievements-box/achievements-box';
 import SignUpForm from './body/sign-up-form/sign-up-form';
 import SignInForm from './body/sign-in-form/sign-in-form';
+import Loading from './loading/loading';
+import './skill-check.scss';
 
 const SkillCheck = () => (
   <BrowserRouter>
@@ -30,6 +31,7 @@ const SkillCheck = () => (
         <Route component={ErrorPage} />
       </Switch>
     </div>
+    <Loading />
   </BrowserRouter>
 );
 
