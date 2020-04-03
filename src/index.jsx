b9f7c3df8dd722abf9bdd2c4bffd6c608fadd48f
Lili Legran <lili_akhunyanova@icloud.com>
2020-04-03 18:49:19 +0300

    feat: redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-// eslint-disable-next-line no-undef
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
+
+import { createStore } from 'redux';
+
+function achievement(state = []) {
+  return state;
+}
+
+const store = createStore(achievement);
+
+store.subscribe(() => {
+  console.log('subscribe', store.getState());
+});
+
+store.dispatch({ type: 'ADD_ACHIEVEMENT', language: 'JavaScript' })
